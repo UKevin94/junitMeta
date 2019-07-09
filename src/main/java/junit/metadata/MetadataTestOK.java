fc248dafd710a34561d8d70b36b8181338e02dc0
Kévin Ung <kung@NX-DOMAIN.LAN>
2019-07-09 14:39:18 +0200

    Uncomment solo key

--- src/main/java/junit/metadata/MetadataTestOK.java
+++ src/main/java/junit/metadata/MetadataTestOK.java
@@ -15,7 +15,7 @@ public class MetadataTestOK {
     }
 
     @Test
-    //@TFMetadata( key = "solo_KEY")
+    @TFMetadata( key = "solo_KEY")
     public void keyOnly(){
         int a = 2;
         int b = 2;
@@ -31,9 +31,17 @@ public class MetadataTestOK {
     }
 
     @Test
-    @TFMetadata( key = "first_KEY", value = {"first_VALUE"})
-    //@TFMetadata( key = "solo")
-    @TFMetadata( key = "list", value = {"first_item", "second_item", "third,_item"})
+    @TFMetadata( key = "multi_KEY_duplicate", value = {"first_LIST", "second_LIST", "first_LIST"})
+    public void keyMultilineValueDuplicatedWarning(){
+        int a = 2;
+        int b = 2;
+        Assertions.assertEquals(a, b);
+    }
+
+    @Test
+    @TFMetadata( key = "first_KEY_annots", value = {"first_VALUE"})
+    @TFMetadata( key = "solo")
+    @TFMetadata( key = "list", value = {"first_item", "second_item", "third_item"})
     public void multiAnnot(){
         int a = 2;
         int b = 2;
@@ -42,7 +50,7 @@ public class MetadataTestOK {
 
     @Test
     @TFMetadata( key = "1111", value = {"22222"})
-    //@TFMetadata( key = "3333")
+    @TFMetadata( key = "3333")
     @TFMetadata( key = "5555", value = {"6666", "7777", "8888"})
     public void numbersEverywhere(){
         int a = 2;
@@ -52,7 +60,7 @@ public class MetadataTestOK {
 
     @Test
     @TFMetadata( key = "first_K-E.Y", value = {"first_VALUE"})
-    //@TFMetadata( key = "se.con-d_KEY")
+    @TFMetadata( key = "se.con-d_KEY")
     @TFMetadata( key = "mu.l-ti_Key", value = {"one", "two", "three"})
     @TFMetadata( key = "first", value = {"fi/r-s.t_VALUE"})
     @TFMetadata( key = "second", value = {"o/ne", "t.wo", "t-hr_ee"})
